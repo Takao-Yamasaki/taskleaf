db5a89a7a6e2cd505eb6d7be235c15c7ba13a45c
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-01 15:42:05 +0900

    オリジナルの検証コードの作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+  
+  # nameがnilの場合に例外が発生するのを避けるため、&.を利用する
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
