6a2c075fe626cf355c38f0dfdad315be78deb28e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-04 09:37:06 +0900

    詳細表示機能のSpecを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+  
+  before do
+    # ユーザーAでブラウザからログインする（前提となっているユーザーを操作しておく）
+    # ログイン画面にアクセスする
+    visit login_path
+    # メールアドレスを入力する
+    fill_in 'メールアドレス', with: login_user.email
+    # パスワードを入力する
+    fill_in 'パスワード', with: login_user.password
+    # 「ログインする」ボタンを押す
+    click_button 'ログインする'
+  end
+  
   describe '一覧表示機能' do
-    let(:user_a) {FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')}
-    let(:user_b) {FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')}
-
-    before do
-      # 作成者がユーザーAであるタスクを作成しておく（テストデータの準備）
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      # ユーザーAでブラウザからログインする（前提となっているユーザーを操作しておく）
-      # ログイン画面にアクセスする
-      visit login_path
-      # メールアドレスを入力する
-      fill_in 'メールアドレス', with: login_user.email
-      # パスワードを入力する
-      fill_in 'パスワード', with: login_user.password
-      # 「ログインする」ボタンを押す
-      click_button 'ログインする'
-    end
-
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
       it 'ユーザーAが作成したタスクが表示される' do
@@ -31,6 +30,20 @@ describe 'タスク管理機能', type: :system do
       let(:login_user) { user_b }
       it 'ユーザーAが作成したタスクが表示されていない' do
         expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
