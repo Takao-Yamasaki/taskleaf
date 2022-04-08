1405705e3fe1e4375a0cd471f7e92d7f9e3dda72
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-08 09:22:46 +0900

    対応するセルの背景色の変更

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,11 @@
+documentaddEventLister('turbolinks:load',function() {
+  document.querySelectorAll('td').forEach(function(td) {
+    td.addEventListener('mouseover',function(e) {
+      e.currentTarget.style.backgroundColor = '#eff';
+    }); 
+    
+    td.addEventListener('mouseout', function(e) {
+      e.currentTarget.style.buckgroundColor = '';
+    });
+  });
+});
